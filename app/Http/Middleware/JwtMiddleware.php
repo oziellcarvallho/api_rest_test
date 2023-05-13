1d7171feedb7effdeecd5ea8fafb6e6a057c935e
oziellcarvallho <ozielcarvalho@live.com>
2023-05-12 22:39:08 -0300

    add jwt middleware

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use JWTAuth;
+use Exception;
+use Illuminate\Http\Response;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+
+use Illuminate\Support\Facades\Auth;
+
+class JwtMiddleware extends BaseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (TokenInvalidException $e) {
+            return response()->json(['error' => 'Invalid token'], Response::HTTP_UNAUTHORIZED);
+        } catch (TokenExpiredException $e) {
+            return response()->json(['error' => 'Token expired'], Response::HTTP_UNAUTHORIZED);
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Token not found'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        if (!$user) {
+            return response()->json(['error' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -21,27 +21,29 @@ Route::group(['middleware' => 'api'], function ($router) {
         Route::post('me', [App\Http\Controllers\Api\ApiAuthController::class, 'me']);
     });
 
-    Route::group(['prefix' => 'project', 'middleware' => 'auth:api'], function ($router) {
-        Route::get('/', [App\Http\Controllers\Api\ApiProjectController::class, 'index']);
-        Route::post('/', [App\Http\Controllers\Api\ApiProjectController::class, 'store']);
-        Route::get('/{project}', [App\Http\Controllers\Api\ApiProjectController::class, 'show']);
-        Route::put('/{project}', [App\Http\Controllers\Api\ApiProjectController::class, 'update']);
-        Route::delete('/{project}', [App\Http\Controllers\Api\ApiProjectController::class, 'destroy']);
-    });
+    Route::group(['middleware' => 'auth.jwt'], function ($router) {
+        Route::group(['prefix' => 'project'], function ($router) {
+            Route::get('/', [App\Http\Controllers\Api\ApiProjectController::class, 'index']);
+            Route::post('/', [App\Http\Controllers\Api\ApiProjectController::class, 'store']);
+            Route::get('/{project}', [App\Http\Controllers\Api\ApiProjectController::class, 'show']);
+            Route::put('/{project}', [App\Http\Controllers\Api\ApiProjectController::class, 'update']);
+            Route::delete('/{project}', [App\Http\Controllers\Api\ApiProjectController::class, 'destroy']);
+        });
 
-    Route::group(['prefix' => 'task', 'middleware' => 'auth:api'], function ($router) {
-        Route::get('/', [App\Http\Controllers\Api\ApiTaskController::class, 'index']);
-        Route::post('/', [App\Http\Controllers\Api\ApiTaskController::class, 'store']);
-        Route::get('/{task}', [App\Http\Controllers\Api\ApiTaskController::class, 'show']);
-        Route::put('/{task}', [App\Http\Controllers\Api\ApiTaskController::class, 'update']);
-        Route::delete('/{task}', [App\Http\Controllers\Api\ApiTaskController::class, 'destroy']);
-    });
+        Route::group(['prefix' => 'task'], function ($router) {
+            Route::get('/', [App\Http\Controllers\Api\ApiTaskController::class, 'index']);
+            Route::post('/', [App\Http\Controllers\Api\ApiTaskController::class, 'store']);
+            Route::get('/{task}', [App\Http\Controllers\Api\ApiTaskController::class, 'show']);
+            Route::put('/{task}', [App\Http\Controllers\Api\ApiTaskController::class, 'update']);
+            Route::delete('/{task}', [App\Http\Controllers\Api\ApiTaskController::class, 'destroy']);
+        });
 
-    Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function ($router) {
-        Route::get('/', [App\Http\Controllers\Api\ApiUserController::class, 'index']);
-        Route::post('/', [App\Http\Controllers\Api\ApiUserController::class, 'store']);
-        Route::get('/{user}', [App\Http\Controllers\Api\ApiUserController::class, 'show']);
-        Route::put('/{user}', [App\Http\Controllers\Api\ApiUserController::class, 'update']);
-        Route::delete('/{user}', [App\Http\Controllers\Api\ApiUserController::class, 'destroy']);
+        Route::group(['prefix' => 'user'], function ($router) {
+            Route::get('/', [App\Http\Controllers\Api\ApiUserController::class, 'index']);
+            Route::post('/', [App\Http\Controllers\Api\ApiUserController::class, 'store']);
+            Route::get('/{user}', [App\Http\Controllers\Api\ApiUserController::class, 'show']);
+            Route::put('/{user}', [App\Http\Controllers\Api\ApiUserController::class, 'update']);
+            Route::delete('/{user}', [App\Http\Controllers\Api\ApiUserController::class, 'destroy']);
+        });
     });
 });
